aa81eb9b44484c4ac098433e70ebf4e9ac42cb82
atps0101 <atps.php@gmail.com>
2023-11-28 18:39:46 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  // mode: 'development',
+  entry: {
+    app: './assets/js/app.js',
+    style: './assets/css/style.css'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]-min.js'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizerPlugin()
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name]-min.css',
+      ignoreOrder: true
+    })
+  ]
+};
